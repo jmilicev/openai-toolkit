315cdcde1c99e8d986e8106aeaca8aca5976d393
Jmilicev <jovan.milicev@gmail.com>
2023-05-25 20:35:21 -0400

    cleanup package.json

--- demo.js
+++ demo.js
@@ -2,10 +2,10 @@ const { call } = require('./caller');
 
 const input = 'what is a crocodile';
 const temperature = 1;
-const maxTokens = 50;
+const maxTokens = 30;
 const modelType = 'gpt-3.5-turbo'; // Replace with your desired model type
 const PARAMETERS = 'a';
-const apiKey = ''; // Replace with your OpenAI API key
+const apiKey = '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'; // Replace with your OpenAI API key
 
 call(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, onData, onEnd);
 
