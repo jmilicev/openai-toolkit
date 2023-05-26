15922cb669f24064dcc14afe3f893a2811c014f5
Jmilicev <jovan.milicev@gmail.com>
2023-05-25 20:35:50 -0400

    apikey

--- demo.js
+++ demo.js
@@ -5,7 +5,7 @@ const temperature = 1;
 const maxTokens = 30;
 const modelType = 'gpt-3.5-turbo'; // Replace with your desired model type
 const PARAMETERS = 'a';
-const apiKey = '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'; // Replace with your OpenAI API key
+const apiKey = ''; // Replace with your OpenAI API key
 
 call(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, onData, onEnd);
 
