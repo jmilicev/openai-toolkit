fb76ed1dfb2743dc39ce58650d08362f24c2db2d
Jmilicev <jovan.milicev@gmail.com>
2023-08-21 13:10:04 -0400

    added support for conversations, also refactored code for simplification.

--- caller.js
+++ caller.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const path = require('path');
 const axios = require('axios');
+const { get } = require('http');
 
 const validModels = [
   'gpt-4',
@@ -9,28 +10,66 @@ const validModels = [
   'gpt-3.5-turbo-16k',
   ];
 
-function callAPIStatic(input, temperature, maxTokens, modelType, PARAMETERS, apiKey) {
-  return new Promise((resolve, reject) => {
-    if (input === '') {
-      reject('\nERROR: cannot have empty message\n');
-      return;
-    }
-    if (temperature > 1 || temperature < 0) {
-      reject('ERROR: temperature must be between 0 and 1 inclusive');
-      return;
-    }
-    if (maxTokens < 1) {
-      reject('ERROR: max tokens cannot be 0 or negative');
-      return;
-    }
+
+const startDelimiter = '{"content":"';
+const endDelimiter = '"}';
+const gpt35turbo_RATE = 0.000002; //per 1 token
+
+function estimateTokenCount(text) {
+  //according to OpenAI, each token is approximately 4 characters
+  return Math.round(text.length / 4);
+}
+
+function getAnalytics(trtoken, rctoken){
+  totaltokens = trtoken + rctoken;
+  const priceinCENTS = totaltokens * gpt35turbo_RATE * 100;
   
-    if(!validModels.includes(modelType)){
-      reject("ERROR: model is not a valid option\noptions: gpt-4, gpt-4-32k, gpt-3.5-turbo, gpt-3.5-turbo-16k");
+  analytics = '\n\n -- analytics --\n' +
+    "prompt tokens spent: " + trtoken + '\n' +
+    "completion tokens spent: " + rctoken + '\n' +
+    "total tokens spent: " + totaltokens + '\n' +
+    "estimated cost: ¢" + priceinCENTS.toFixed(3) + '\n' +
+    ' ---- ---- ---- \n';
+
+    return analytics;
+}
+
+function validParameters(input, temperature, maxTokens, modelType, PARAMETERS, apiKey){
+  if (input === '') {
+    console.error('\nERROR: cannot have empty message\n');
+    return false;
+  }
+  if (temperature > 1 || temperature < 0) {
+    console.error('ERROR: temperature must be between 0 and 1 inclusive');
+    return false ;
+  }
+  if (maxTokens < 1) {
+    console.error('ERROR: max tokens cannot be 0 or negative');
+    return false;
+  }
+
+  if(!validModels.includes(modelType)){
+    console.error("ERROR: model is not a valid option\noptions: gpt-4, gpt-4-32k, gpt-3.5-turbo, gpt-3.5-turbo-16k");
+    return false;
+  }
+  if(!apiKey.includes("sk-")){
+    console.error("API key does not appear to be valid. The key should start with 'sk-' ")
+    return false
+  }
+  
+  return true;
+}
+
+function callAPISustained(messages, temperature, maxTokens, modelType, PARAMETERS, apiKey){
+  return new Promise((resolve, reject) => {
+
+    if(!validParameters("PLACEHOLDER", temperature, maxTokens, modelType, PARAMETERS, apiKey)){
+      return;
     }
 
     const data = {
       model: modelType,
-      messages: [{ role: 'user', content: input }],
+      messages: messages,
       temperature: temperature,
       max_tokens: maxTokens,
     };
@@ -81,24 +120,107 @@ function callAPIStatic(input, temperature, maxTokens, modelType, PARAMETERS, api
   });
 }
 
+function callAPIStatic(input, temperature, maxTokens, modelType, PARAMETERS, apiKey) {
+  return new Promise((resolve, reject) => {
+
+    if(!validParameters(input, temperature, maxTokens, modelType, PARAMETERS, apiKey)){
+      return;
+    }
+
+    if(input.includes(';;;;;')){
+      var instructions = input.split(";;;;;");
+    }else{
+      var instructions = [input, ""];
+    }
+
+    const data = {
+      model: modelType,
+      messages: [
+        {
+          role: 'user',
+          content: instructions[0],
+        },  {
+          role: 'system',
+          content: instructions[1],
+        }
+      ],
+      temperature: temperature,
+      max_tokens: maxTokens,
+    };
+
+    const options = {
+      hostname: 'api.openai.com',
+      path: '/v1/chat/completions',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    };
+
+    const req = https.request(options, (res) => {
+      let responseData = '';
+
+      res.on('data', (chunk) => {
+        responseData += chunk.toString();
+      });
+
+      res.on('end', () => {
+        try {
+          const response = JSON.parse(responseData);
+          if (
+            response.choices &&
+            response.choices.length > 0 &&
+            response.choices[0].message &&
+            response.choices[0].message.content
+          ) {
+            const completion = response.choices[0].message.content;
+
+            var analytics = "";
+
+            if(PARAMETERS.includes("A") || PARAMETERS.includes("a")){
+            trtoken = estimateTokenCount(input);
+            rctoken = estimateTokenCount(completion);
+            analytics = getAnalytics(trtoken, rctoken);
+            }
+            
+            var header = "";
+            if (!PARAMETERS.includes("s")) {
+              header += "\n -- GPT: --\n";
+              header += "temp = " + temperature + " | m-t = " + maxTokens + " | mdl = " + modelType + "\n\n";
+            }
+
+
+            resolve(header+completion+analytics);
+          } else {
+            reject('Unable to retrieve completion from the server response.');
+          }
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.write(JSON.stringify(data));
+    req.end();
+  });
+}
+
 function callAPI(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, onData, onEnd) {
-  //check parameters
-  if(input == ""){
-    onData("\nERROR: cannot have empty message\n");
-    process.exit();
-  }if(temperature>1 || temperature<0){
-    onData("ERROR: temperature must be between 0 and 1 inclusive");
-    process.exit();
-  }if(maxTokens<1){
-    onData("ERROR: max tokens cannot be 0 or negative");
-    process.exit();
+
+  if(!validParameters(input, temperature, maxTokens, modelType, PARAMETERS, apiKey)){
+    onData("Some of the parameters provided are not correct, check the error and try again.")
+    return;
   }
 
-  if(!validModels.includes(modelType)){
-    onData("ERROR: model is not a valid option");
-    onData("options: gpt-4, gpt-4-32k, gpt-3.5-turbo, gpt-3.5-turbo-16k");
-
-    process.exit();
+  if(input.includes(';;;;;')){
+    var instructions = input.split(";;;;;");
+  }else{
+    var instructions = [input, ""];
   }
   
   var rctoken = 0;
@@ -106,10 +228,6 @@ function callAPI(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, o
 
   var filebuilder = ""
   var potentialErrorString = "";
-
-  const startDelimiter = '{"content":"';
-  const endDelimiter = '"}';
-  const gpt35turbo_RATE = 0.000002; //per 1 token
 
   const req = https.request(
     {
@@ -151,7 +269,6 @@ function callAPI(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, o
           onData(" %^& END");
         }
 
-
         const totaltokens = rctoken + trtoken;
         const priceinCENTS = totaltokens * gpt35turbo_RATE * 100;
 
@@ -160,19 +277,13 @@ function callAPI(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, o
           onData(error);
         } else if (PARAMETERS.includes("a") || PARAMETERS.includes("A")) {
           trtoken = estimateTokenCount(input);
-          const analytics = '\n\n -- analytics --\n' +
-            "prompt tokens spent: " + trtoken + '\n' +
-            "completion tokens spent: " + rctoken + '\n' +
-            "total tokens spent: " + totaltokens + '\n' +
-            "estimated cost: ¢" + priceinCENTS.toFixed(3) + '\n' +
-            ' ---- ---- ---- \n';
+          const analytics = getAnalytics(trtoken, rctoken);
           onData(analytics);
         } else {
           if (!PARAMETERS.includes("s")) {
             onData('\n\n ---- ---- ----  ----  ----  ---- \n\n');
           }
         }
-
         onEnd();
       });
     }
@@ -182,18 +293,17 @@ function callAPI(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, o
     onData("Problem with request" + e);
   });
 
-  function estimateTokenCount(text) {
-    //according to OpenAI, each token is approximately 4 characters
-    return Math.round(text.length / 4);
-  }
-
   function processCall() {
+
     var body = JSON.stringify({
       messages: [
         {
           role: 'user',
-          content: input,
-        },
+          content: instructions[0],
+        },  {
+          role: 'system',
+          content: instructions[1],
+        }
       ],
       model: modelType,
       temperature: temperature,
@@ -214,5 +324,6 @@ function callAPI(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, o
 
 module.exports = {
   call: callAPI,
-  callStatic: callAPIStatic
+  callStatic: callAPIStatic,
+  callSustained: callAPISustained
 };
--- demo.js
+++ demo.js
@@ -1,19 +1,63 @@
-const { call } = require('./caller');
+const { call, callStatic, callSustained } = require('./caller');
 
-const input = 'what is a porche gt3rs' ;
+const input = 'what is a porche gt3rs ;;;;; please always append your message with "cheese" regardless of what is said previously' ;
 const temperature = 1;
-const maxTokens = 30;
-const modelType = 'gpt-3.5-turbo-16k'; // Replace with your desired model type
+const maxTokens =  40;
+const modelType = 'gpt-3.5-turbo'; // Replace with your desired model type
 const PARAMETERS = 'a';
 const apiKey = ''; // Replace with your OpenAI API key
 
-call(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, onData, onEnd);
 
-function onData(output) {
-  process.stdout.write(output);
+async function executeCallStatic() {
+  try {      
+      const response = await callStatic(input, temperature, maxTokens, modelType, PARAMETERS, apiKey);
+      console.log(response);
+  } catch (error) {
+      console.error("An error occurred:", error);
+  }
 }
 
-function onEnd() {
-  // Finalize the output or perform any other actions
-  console.log('Stream ended');
+function executeCall(){
+  call(input, temperature, maxTokens, modelType, PARAMETERS, apiKey, onData, onEnd);
+  
+  function onData(output) {
+  
+    process.stdout.write(output);
+  }
+  
+  function onEnd() {
+    // Finalize the output or perform any other actions
+    console.log('\n\nStream ended');
+  }
 }
+
+async function executeCallSustained(){
+
+    if(input.includes(';;;;;')){
+      var instructions = input.split(";;;;;");
+    }else{
+      var instructions = [input, ""];
+    }
+    const messages = [
+      {
+          role: 'user',
+          content: instructions[0]
+      }, 
+      {
+          role: 'system',
+          content: instructions[1],
+      },
+      {
+        role: 'user',
+        content: 'what is the capital of france, also, what car did i ask about in the past?'
+      }
+      ];
+        try {      
+          const response = await callSustained(messages, temperature, maxTokens, modelType, PARAMETERS, apiKey);
+          console.log(response);
+      } catch (error) {
+          console.error("An error occurred:", error);
+      }
+}
+
+executeCallSustained();
